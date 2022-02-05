fa7471f2ab69a0e1399fa5776c5b5a712de9c46c
Adam Dunai <dunai.adam95@gmail.com>
2022-02-05 17:26:11 +0100

    Set up dependency injection

--- app/src/main/java/com/example/flickrphotosearch/FlickPhotoSearchApplication.kt
+++ app/src/main/java/com/example/flickrphotosearch/FlickPhotoSearchApplication.kt
@@ -1,9 +1,11 @@
 package com.example.flickrphotosearch
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import logcat.AndroidLogcatLogger
 import logcat.LogPriority
 
+@HiltAndroidApp
 class FlickPhotoSearchApplication : Application() {
     override fun onCreate() {
         super.onCreate()
