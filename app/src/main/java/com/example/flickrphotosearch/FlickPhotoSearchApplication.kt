80f2174f80705990bb4e16dab18e51817ac00651
Adam Dunai <dunai.adam95@gmail.com>
2022-02-05 17:10:26 +0100

    Set up logging

--- app/src/main/java/com/example/flickrphotosearch/FlickPhotoSearchApplication.kt
+++ app/src/main/java/com/example/flickrphotosearch/FlickPhotoSearchApplication.kt
@@ -1,5 +1,13 @@
 package com.example.flickrphotosearch
 
 import android.app.Application
+import logcat.AndroidLogcatLogger
+import logcat.LogPriority
 
-class FlickPhotoSearchApplication : Application()
+class FlickPhotoSearchApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AndroidLogcatLogger.installOnDebuggableApp(this, LogPriority.VERBOSE)
+    }
+}
