56cbf60778fd4a15b2ffe5611cb5286c0ffd58a8
Adam Dunai <dunai.adam95@gmail.com>
2022-02-11 17:06:42 +0100

    Refactor navigation argument handling

--- app/src/main/java/com/example/flickrphotosearch/main/ui/viewmodel/PhotoDetailsViewModel.kt
+++ app/src/main/java/com/example/flickrphotosearch/main/ui/viewmodel/PhotoDetailsViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.flickrphotosearch.main.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.flickrphotosearch.common.mapper.toUiModel
@@ -14,20 +15,31 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PhotoDetailsViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
     private val repository: PhotoRepository,
 ) : ViewModel() {
+
+    companion object {
+        private const val KEY_PHOTO_ID = "photoId"
+    }
+
+    init {
+        getPhotoDetails()
+    }
 
     private val _uiState = MutableStateFlow<PhotoDetailsUiState>(PhotoDetailsUiState.Empty)
     val uiState: StateFlow<PhotoDetailsUiState> = _uiState
 
-    fun getPhotoDetails(photoId: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            _uiState.value = PhotoDetailsUiState.Loading
-            try {
-                val response = repository.getPhotoDetails(photoId)
-                _uiState.value = PhotoDetailsUiState.Success(response.toUiModel())
-            } catch (exception: Exception) {
-                _uiState.value = PhotoDetailsUiState.Error
+    fun getPhotoDetails() {
+        savedStateHandle.get<String>(KEY_PHOTO_ID)?.let { photoId ->
+            viewModelScope.launch(Dispatchers.IO) {
+                _uiState.value = PhotoDetailsUiState.Loading
+                try {
+                    val response = repository.getPhotoDetails(photoId)
+                    _uiState.value = PhotoDetailsUiState.Success(response.toUiModel())
+                } catch (exception: Exception) {
+                    _uiState.value = PhotoDetailsUiState.Error
+                }
             }
         }
     }
